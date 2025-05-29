748756f5299242734f21f16ab3a7c5d6896d1fb9
Filipe Queiroz <filipe.queiroz.abreu@gmail.com>
2025-05-29 17:16:35 -0300

    analises diarias

--- Bitcoin/pegar_historico.py
+++ Bitcoin/pegar_historico.py
@@ -1,12 +1,28 @@
 import pandas as pd
 from binance.client import Client
 
-
-
 def pegar_historico(par, intervalo, data_inicio):
 
     client = Client()
     klines = client.get_historical_klines(par, intervalo, data_inicio)
+    
+    df = pd.DataFrame(klines, columns=[
+    "timestamp", "open", "high", "low", "close", "volume",
+    "close_time", "quote_asset_volume", "number_of_trades",
+    "taker_buy_base_asset_volume", "taker_buy_quote_asset_volume", "ignore"
+    ])
+
+    # Converter timestamp para datetime
+    df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
+
+    cols = ["open", "high", "low", "close", "volume"]
+    df[cols] = df[cols].astype(float)
+    return df
+
+def dados_historicos_recentes(par, data_inicio, limite):
+
+    client = Client()
+    klines = client.get_klines(symbol=par, interval=data_inicio, limit=limite)
     
     df = pd.DataFrame(klines, columns=[
     "timestamp", "open", "high", "low", "close", "volume",
